3b5714f8ac3788f933236b05353b2b5973d93a09
Eniyan-GB <eniyan.b_25cs@kgkite.ac.in>
2026-01-30 21:01:46 +0530

    Update problem_60.py

--- problem_60.py
+++ problem_60.py
@@ -2,11 +2,5 @@
 # Find and fix the error
 
 def is_armstrong(n):
-    num_str = str(n)
-    num_digits = len(num_str)
-    total = 0
-    for digit in num_str:
-        total += int(digit) ** num_digits
-    return total == n
-
-print(f"Is 153 Armstrong? {is_armstrong(153)}")
+    num_digits = len(str(n))
+    return sum(int(d)**num_digits for d in str(n)) == n
