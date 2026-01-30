acb70ba8fba4d97c7e6b173d71dbf8b308893ec8
Eniyan-GB <eniyan.b_25cs@kgkite.ac.in>
2026-01-30 20:10:33 +0530

    Update problem_14.py

--- problem_14.py
+++ problem_14.py
@@ -4,9 +4,12 @@
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, n):
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
 
-print(f"Is 17 prime? {is_prime(17)}")
