7c2c7360812f022fa37799f6412aacbbc2c51f27
Eniyan-GB <eniyan.b_25cs@kgkite.ac.in>
2026-01-30 20:01:58 +0530

    Update problem_10.py

--- problem_10.py
+++ problem_10.py
@@ -1,9 +1,6 @@
 # Problem 10: Remove duplicates from a list
 # Find and fix the error
-
-numbers = [1, 2, 2, 3, 4, 4, 5]
 unique = []
-for num in numbers:
-    if num not in unique:
-        unique.append(num)
-print(f"Unique numbers: {unique}")
+[unique.append(x) for x in numbers if x not in unique]
+
+
